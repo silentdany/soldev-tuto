eb7b598388dfee4f6025c379f2b7af7972b56d05
a.dagnicourt <dagnicourt.a@gmail.com>
2022-05-12 13:55:15 +0200

    init: module 1.5 done (deserializeInstructions)

--- components/MovieList/MovieList.tsx
+++ components/MovieList/MovieList.tsx
@@ -1,21 +1,39 @@
 import { FC, useEffect, useState } from "react";
+import * as web3 from "@solana/web3.js";
+
 import { Movie } from "../../models/Movie";
 import { MovieCard } from "./Card";
 
 const MOVIE_REVIEW_PROGRAM_ID = "CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN";
 
 export const MovieList: FC = () => {
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
-    setMovies(Movie.mocks);
+    connection
+      .getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID))
+      .then(async (accounts) => {
+        const movies: Movie[] = accounts.reduce(
+          (accum: Movie[], { pubkey, account }) => {
+            const movie = Movie.deserialize(account.data);
+            if (!movie) {
+              return accum;
+            }
+
+            return [...accum, movie];
+          },
+          []
+        );
+        setMovies(movies);
+      });
   }, []);
 
   return (
     <div>
-      {movies.map((movie, i) => {
-        return <MovieCard key={i} movie={movie} />;
-      })}
+      {movies.map((movie, i) => (
+        <MovieCard key={i} movie={movie} />
+      ))}
     </div>
   );
 };
--- components/StudentIntroList/StudentIntrolList.tsx
+++ components/StudentIntroList/StudentIntrolList.tsx
@@ -1,14 +1,32 @@
 import { FC, useEffect, useState } from "react";
+import * as web3 from "@solana/web3.js";
+
 import { StudentIntro } from "../../models/StudentIntro";
 import { StudentCard } from "./Card";
 
 const STUDENT_INTRO_PROGRAM_ID = "HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf";
 
 export const StudentIntroList: FC = () => {
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
   const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([]);
 
   useEffect(() => {
-    setStudentIntros(StudentIntro.mocks);
+    connection
+      .getProgramAccounts(new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID))
+      .then(async (accounts) => {
+        const studentIntro: StudentIntro[] = accounts.reduce(
+          (accum: StudentIntro[], { pubkey, account }) => {
+            const studentIntro = StudentIntro.deserialize(account.data);
+            if (!studentIntro) {
+              return accum;
+            }
+
+            return [...accum, studentIntro];
+          },
+          []
+        );
+        setStudentIntros(studentIntro);
+      });
   }, []);
 
   return (
